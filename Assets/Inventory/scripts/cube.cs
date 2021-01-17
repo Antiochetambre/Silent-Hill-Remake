4668d4a169596e4777996dfd959250c275916611
Asım Öner <asimoner11@gmail.com>
2021-01-17 15:29:55 +0300

    Inventory added.

---
+++ Assets/Inventory/scripts/cube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementX = Input.GetAxis("Horizontal");
+        float movementY = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(movementX, 0.0f, movementY);
+
+        transform.position += direction * speed;
+        
+
+
+    }
+}
